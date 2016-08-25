470e4a6dfddaa50b9afd850efd592230bb60f3f9
Kosyanenko Aleksey <lehakos@gmail.com>
2016-08-25 12:14:28 +0300

    start to work on lines

---
+++ markup/components/line/data/data.js
@@ -0,0 +1,17 @@
+var data = {line: {
+    home: {
+        name: 'home'
+    },
+
+    about: {
+        name: 'about'
+    },
+
+    mission: {
+        name: 'mission'
+    },
+
+    team: {
+        name: 'team'
+    }
+}}
