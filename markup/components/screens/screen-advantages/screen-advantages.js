70a7320a9556d4cf2c60aae57429a447ef5ae536
Kosyanenko Aleksey <lehakos@gmail.com>
2016-08-25 13:12:03 +0300

    add plane for screen-advantages

--- markup/components/screens/screen-advantages/screen-advantages.js
+++ markup/components/screens/screen-advantages/screen-advantages.js
@@ -9,10 +9,20 @@ export class ScreenAdvantages extends Component {
         if (this._ready) return this;
         this._ready = true;
 
+        this.firstShow = true;
+
         this.block.addEventListener('show', this._onShow.bind(this));
     }
 
     _onShow(e) {
         header.toggleNavClasses('header_nav_right');
+
+        if (this.firstShow) {
+            let plane = this.block.querySelector('.screen-advantages__plane');
+
+            plane.classList.add('screen-advantages__plane_trigger');
+
+            this.firstShow = false;
+        }
     }
 }
