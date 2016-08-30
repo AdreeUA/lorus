14cb8ff9aae831fa8f1c7091eafba3be33fdd8f8
Kosyanenko Aleksey <lehakos@gmail.com>
2016-08-30 18:26:37 +0300

    start work on line script

--- markup/components/line/line.js
+++ markup/components/line/line.js
@@ -3,7 +3,23 @@ import { Component } from 'helpers-js';
 export class Line extends Component {
     constructor(block) {
         super(block, 'line', function() {
+            this.type = this.block.classList.contains('line_css') ? 'css' : 'svg';
+            this.static = this.block.querySelector('.line__icon_static');
+            this.active = this.block.querySelector('.line__icon_active');
 
-        });
+            if (this.type === 'svg') {
+                this._pathPrepare(this.active);
+            } else {
+                this.active.style.width = 0;
+            }
+        })
+    }
+
+    _pathPrepare(elem) {
+        console.log(elem);
+        let lineLength = elem.getTotalLength();
+
+        elem.style.strokeDasharray = lineLength;
+        elem.style.strokeDashoffset =  lineLength;
     }
 }
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -4,6 +4,7 @@ import { Component } from 'helpers-js';
 
 import { Screens } from 'components/screens/screens';
 import { Slider } from 'components/slider/slider';
+import { Line } from 'components/line/line';
 import { InfoList } from 'components/info/info-list/info-list';
 
 'use strict';
@@ -12,6 +13,7 @@ $(() => {
     polyfills.init();
     // ================ Здесь инициализируем модули =====================
 
+    Component.init('.line', Line);
     Component.init('.screens', Screens);
     Component.init('.slider', Slider);
     Component.init('.info-list', InfoList);
