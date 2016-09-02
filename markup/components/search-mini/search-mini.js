b3359dae3b06b31a16dc20d2eb76352b825aa86a
Kosyanenko Aleksey <lehakos@gmail.com>
2016-09-02 15:01:57 +0300

    fix lines size in IE

--- markup/components/search-mini/search-mini.js
+++ markup/components/search-mini/search-mini.js
@@ -5,25 +5,25 @@ export class SearchMini extends Component {
         super(block, 'search-mini', function() {
             this.btn = this.block.querySelector('.search-mini__submit');
 
-            this._init();
+            // this._init();
         });
     }
 
-    _init() {
-        this.btn.setAttribute('disabled', 'disabled');
-
-        this.block.addEventListener('keyup', this._onKeyup.bind(this));
-    }
-
-    _onKeyup(e) {
-        let field = e.target.closest('.search-mini__field');
-
-        if (field) {
-            if (field.value) {
-                this.btn.removeAttribute('disabled');
-            } else {
-                this.btn.setAttribute('disabled', 'disabled');
-            }
-        };
-    }
+    // _init() {
+    //     this.btn.setAttribute('disabled', 'disabled');
+    //
+    //     this.block.addEventListener('keyup', this._onKeyup.bind(this));
+    // }
+    //
+    // _onKeyup(e) {
+    //     let field = e.target.closest('.search-mini__field');
+    //
+    //     if (field) {
+    //         if (field.value) {
+    //             this.btn.removeAttribute('disabled');
+    //         } else {
+    //             this.btn.setAttribute('disabled', 'disabled');
+    //         }
+    //     };
+    // }
 }
