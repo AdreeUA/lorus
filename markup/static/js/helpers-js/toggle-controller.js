6c6d4f172c1832aee79861da0c01a29aa7fa6e40
Kosyanenko Aleksey <lehakos@gmail.com>
2016-09-20 15:51:03 +0300

    fix toggle-controller function, remove min-height from screen-advantages__text-block on mobile

--- markup/static/js/helpers-js/toggle-controller.js
+++ markup/static/js/helpers-js/toggle-controller.js
@@ -2,24 +2,26 @@ import { media } from './variables';
 
 export const toggleController = (controller1, controller2) => {
     if (matchMedia(media.tablet).matches) {
-        if (!controller1.enabled()) return;
+        if (controller1.enabled()) {
+            TweenMax.set('.js-tween', { clearProps: 'all' });
+            controller1.enabled(false);
+            controller1.update();
+        }
 
-        TweenMax.set('.js-tween', { clearProps: 'all' });
-        controller1.enabled(false);
-        controller1.update();
-
-        if (controller2) {
+        if (controller2 && !controller2.enabled()) {
             controller2.enabled(true);
             controller2.update(true);
         }
 
-    } else if (!controller1.enabled()) {
+    } else {
 
-        TweenMax.set('.js-tween', { clearProps: 'all' });
-        controller1.enabled(true);
-        controller1.update(true);
+        if (!controller1.enabled()) {
+            TweenMax.set('.js-tween', { clearProps: 'all' });
+            controller1.enabled(true);
+            controller1.update(true);
+        }
 
-        if (controller2) {
+        if (controller2 && controller2.enabled()) {
             controller2.enabled(false);
             controller2.update();
         }
