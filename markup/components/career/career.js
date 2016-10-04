e8a2dc9806ca2fea9dcc12537a5ff10d9d058bfc
Виталий Никитин <v.nikitin@digitalwand.ru>
2016-10-04 13:09:39 +0500

    career started

---
+++ markup/components/career/career.js
@@ -0,0 +1,13 @@
+import { Component } from 'helpers-js';
+
+import { ThroughService } from './through-career/through-career';
+
+export class Service extends Component {
+    constructor(block) {
+        super(block, 'career', function() {});
+    }
+}
+
+Service.initCurrentService = () => {
+    Component.init(document.querySelector('.through-career'), ThroughService);
+}
