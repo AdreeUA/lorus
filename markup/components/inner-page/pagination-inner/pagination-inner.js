37d4ed686db08bbe3da16936b90906c5171b5f2b
Kosyanenko Aleksey <lehakos@gmail.com>
2016-10-10 20:36:57 +0300

    move components from inner-page folder

--- markup/components/inner-page/pagination-inner/pagination-inner.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'helpers-js';
-
-export class  extends Component {
-    constructor(block) {
-        super(block, 'pagination-inner');
-        if (this._ready) return this;
-        this._ready = true;
-    }
-}
--- markup/components/inner-page/vacancies-inner/data/data.js
+++
@@ -1,1 +0,0 @@
-var data = {vacancies-inner: {}}
