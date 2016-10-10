a1769e15b34079872780f421b9e7313f8e98ac2e
Zmievskoy Andrey <a.zmievskoy@digitalwand.ru>
2016-10-10 17:30:13 +0300

    adaptiv pages 404 and contacts

--- markup/components/map/map.js
+++ markup/components/map/map.js
@@ -8,10 +8,31 @@ export class Map extends Component {
     }
 
     init() {
-        const map = new ymaps.Map('map', {
-            center: [55.76, 37.64],
+        var map = new ymaps.Map('map', {
+            center: [55.76, 37.602],
             zoom: 15,
             controls: []
         });
+
+        var MyBalloonLayout = ymaps.templateLayoutFactory.createClass(
+            '<div class="mark">'+
+            '<div class="mark__inner">'+
+            '<div class="mark__title">Головной офис в Москве</div>'+
+            '<div class="mark__content">'+
+            '<div class="mark__item">Ул. Тестовская, дом 10, подъезд 2 (Северная башня)</div>'+
+            '<div class="mark__item"><a href="mailto:info@lorus-scm.com" class="mark__email link link_theme_fill">info@lorus-scm.com</a></div>'+
+            '<div class="mark__item"><a href="tel:+74952128506" class="mark__phone">+7 495 212-85-06</a></div>'+
+            '</div>'+
+            '</div>'+
+            '</div>'
+        );
+
+        var myPlacemark = new ymaps.Placemark([55.764286, 37.581408], {}, {
+            balloonLayout: MyBalloonLayout
+        });
+
+        map.geoObjects.add(myPlacemark);
+        myPlacemark.balloon.open([55.76, 37.64]);
+
     }
 }
