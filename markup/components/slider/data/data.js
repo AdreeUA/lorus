3c9fa2aacf027845d16e220049d923c3e84a03d2
Kosyanenko Aleksey <lehakos@gmail.com>
2016-10-12 15:43:11 +0300

    complete academy team mobile

--- markup/components/slider/data/data.js
+++ markup/components/slider/data/data.js
@@ -1,1 +1,11 @@
-var data = {slider: {}}
+var data = {slider: {
+    academyTeam: {
+        settings: {
+            "variableWidth": true,
+            "dots": false,
+            "infinite": false,
+            "arrows": false
+        },
+        destroy: '(min-width: 1024px)'
+    }
+}}
